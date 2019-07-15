a5c31f17635bb7e594bb39b85c3ea76c96a9f8ff
Brian Wilson <i9_editor@Brians-MacBook-Pro-2.local>
2019-07-15 18:56:50 -0500

    first commit

---
+++ scripts/ChickenMonkey.js
@@ -0,0 +1,16 @@
+for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
+    if (currentNumber % 5 === 0 && currentNumber % 7 === 0) {
+        console.log("ChickenMonkey") 
+     } else if (currentNumber % 7 === 0)  {
+        console.log("Monkey")
+     } else if (currentNumber % 5 === 0) {
+        console.log("Chicken") 
+    } else {
+        console.log(currentNumber)
+    }
+
+}
+
+
+
+
